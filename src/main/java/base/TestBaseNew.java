e3e8833af36a4cecab9a0e25da84967c26442d0c
autom99 <ehardik92@gmail.com>
2020-10-20 14:34:46 +0530

    Final commit on 20-Oct-2020.

--- src/main/java/base/TestBaseNew.java
+++ src/main/java/base/TestBaseNew.java
@@ -1,10 +1,11 @@
 package base;
 
+import TestUtil.Constants;
+import TestUtil.WebEventListener;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,15 +14,10 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import utility.Constants;
-import utility.WebEventListener;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
-public class TestBaseNew{
+public class TestBaseNew {
 
     private WebDriver driver;
     private EventFiringWebDriver e_driver;
@@ -32,33 +28,34 @@ public class TestBaseNew{
 
     @BeforeClass()
     public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+		options.addArguments("-incognito");
+        options.addArguments("start-maximized");
 
-        // Initializing instance of Firefox WebDriver
         System.setProperty("webdriver.chrome.driver", Constants.PROJECTPATH + "\\drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, waitTime);
 
-        // Initializing EventFiringWebDriver using Firefox WebDriver instance
         e_driver = new EventFiringWebDriver(driver);
 
-        // Now create object of EventListerHandler to register it with EventFiringWebDriver
         eventListener = new WebEventListener();
         e_driver.register(eventListener);
+        driver = e_driver;
 
         e_driver.manage().window().maximize();
         e_driver.get(Constants.BASEURL);
     }
 
-    @Test
+   @Test
     public void testLogin()  {
         System.out.println("***** Executing Test Login ***** ");
         //Entering user name and clicking on next
-        e_driver.findElement(By.xpath("//input[@id='emp_code']")).sendKeys("username");
+        driver.findElement(By.xpath("//input[@id='emp_code']")).sendKeys("username");
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='Password']")));
         //Entering password and clicking on sign-in
-        e_driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("password");
-        e_driver.findElement(By.xpath("//button[@class='login100-form-btn']")).click();
+        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("password");
+        driver.findElement(By.xpath("//button[@class='login100-form-btn']")).click();
 
         String exp_URL =  Constants.BASEURL + "Home/Index";
         String act_URL = driver.getCurrentUrl();
